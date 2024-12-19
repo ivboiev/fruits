e185d4b68ff008b9c22100773c224357ea6869c8
ivboiev <146440455+ivboiev@users.noreply.github.com>
2024-12-19 10:22:20 +0100

    Add files via upload
    He subido todos archivos

---
+++ app.js
@@ -0,0 +1,69 @@
+const { createApp } = Vue;
+
+createApp({
+  template: `
+    <div>
+      <h1>Lista de Frutas</h1>
+      <ul>
+        <li v-for="fruta in paginatedCities" :key="fruta.id" >
+          <img :src="fruta.imagen" :alt="fruta.fruta" width="100" height="100" />
+          <div>
+            <strong>{{ fruta.fruta }}</strong> ({{ fruta.precioKg.toLocaleString() }} precio por kilo)
+            <br />
+           
+          </div>
+        </li>
+      </ul>
+
+      <div class="pagination">
+        <button @click="prevPage" :disabled="currentPage === 1">Anterior</button>
+        <span>Página {{ currentPage }} de {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Siguiente</button>
+      </div>
+    </div>
+
+
+   
+  `,
+  data() {
+    return {
+      cities: [
+        { id: 1, fruta: "Manzana", precioKg: "2.50€", imagen: "img/apple.webp" },
+        { id: 2, fruta: "Avocado", precioKg: "8€", imagen: "img/avocado.webp" },
+        { id: 3, fruta: "Banana", precioKg: "2€", imagen: "img/banana.webp" },
+        { id: 4, fruta: "Mora", precioKg: "9€", imagen: "img/blackberries.webp" },
+        { id: 5, fruta: "Arándanos", precioKg: "7.60€", imagen: "img/blueberries.webp" },
+        { id: 6, fruta: "Cereza", precioKg: "6.50€", imagen: "img/cherries.webp" },
+        { id: 7, fruta: "Pomelo", precioKg: "2.50€", imagen: "img/grapefruit.webp" },
+        { id: 8, fruta: "Uva", precioKg: "6.50€", imagen: "img/grapes.webp" },
+        { id: 9, fruta: "Kiwi", precioKg: "4.75€", imagen: "img/kiwi.webp" },
+        { id: 10, fruta: "Naranja", precioKg: "2€", imagen: "img/orange.webp" },
+        { id: 11, fruta: "Melocón", precioKg: "3.25€", imagen: "img/peach.webp" },
+        { id: 12, fruta: "Pera", precioKg: "2.75€", imagen: "img/pears.webp" },
+        { id: 13, fruta: "Ciruelas", precioKg: "2.25€", imagen: "img/plums.webp" },
+        { id: 14, fruta: "Grandas", precioKg: "7€", imagen: "img/pomegranates.webp" },
+        { id: 15, fruta: "Fresa", precioKg: "7.25€", imagen: "img/strawberries.webp" }
+      ],
+      currentPage: 1,
+      itemsPerPage: 5,
+    };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.cities.length / this.itemsPerPage);
+    },
+    paginatedCities() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.cities.slice(start, end);
+    },
+  },
+  methods: {
+    nextPage() {
+      if (this.currentPage < this.totalPages) this.currentPage++;
+    },
+    prevPage() {
+      if (this.currentPage > 1) this.currentPage--;
+    },
+  },
+}).mount('#app');
